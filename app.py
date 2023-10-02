e7ab1106087ccc29c2493901577b9ec45c6dae66
Amenallah berrejeb <90092833+berrejebamen@users.noreply.github.com>
2023-10-02 23:39:46 +0100

    Update app.py

--- app.py
+++ app.py
@@ -164,21 +164,21 @@ def txt4(a, b):
     st.markdown(f'<p style="font-size: 25px; color: red;"><code>{b}</code></p>', unsafe_allow_html=True)
 
 #####################
-"""def add_bg_from_local(image_file):
-    with open(image_file, "rb") as image_file:
-        encoded_string = base64.b64encode(image_file.read())
-    st.markdown(
-    f"""
-    <style>
-    .stApp {{
-        background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
-        background-size: cover
-    }}
-    </style>
-    """,
-    unsafe_allow_html=True
-    )
-add_bg_from_local('bg.png')  """ 
+#"""def add_bg_from_local(image_file):
+    #with open(image_file, "rb") as image_file:
+        #encoded_string = base64.b64encode(image_file.read())
+    #st.markdown(
+    #f"""
+    #<style>
+    #.stApp {{
+        #background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
+        #background-size: cover
+    #}}
+    #</style>
+    #""",
+    #unsafe_allow_html=True
+    #)
+#add_bg_from_local('bg.png')   
 
 
 # Sidebar: If using streamlit_option_menu
