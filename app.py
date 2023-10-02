7208972d32a3b0ac86f32e9032a6744348a5fad6
Amenallah berrejeb <90092833+berrejebamen@users.noreply.github.com>
2023-10-02 23:05:35 +0100

    Update app.py

--- app.py
+++ app.py
@@ -178,7 +178,7 @@ def add_bg_from_local(image_file):
     """,
     unsafe_allow_html=True
     )
-/add_bg_from_local('bg.png') 
+add_bg_from_local('bg.png') 
 
 
 # Sidebar: If using streamlit_option_menu
